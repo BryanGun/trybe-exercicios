78df85a89ef0d6f87b35912a02e1b0c0fcb2880a
Bryan_Adriel <badriel62@gmail.com>
2023-06-05 18:33:45 -0300

    subindo 7-2

---
+++ fundamentos/secao-7-Higher-Order-Functions-do-JavaScript-ES6/Dia-2:Higher-Order-Functions-sort-e-map/map.js
@@ -0,0 +1,10 @@
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+console.log(persons);
+console.log(fullNames);
---
+++ fundamentos/secao-7-Higher-Order-Functions-do-JavaScript-ES6/Dia-2:Higher-Order-Functions-sort-e-map/map3.js
@@ -0,0 +1,10 @@
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+const atualizaProdutos = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+
+const productsList = atualizaProdutos(products, prices);
+console.log(productsList);
